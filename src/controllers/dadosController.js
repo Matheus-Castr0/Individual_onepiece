14b8cebcc6c4d0f6c28310987ab5f877e50273df
matheuscastro <matheus.csilva@sptech.school>
2024-05-23 14:22:36 -0300

    corrigindo erro

--- src/controllers/dadosController.js
+++ src/controllers/dadosController.js
@@ -1,17 +1,15 @@
 var dadosModel = require("../models/dadosModel")
 
 function qtdEscolhido(req, res) {
-    for (var i = 1; i <= 10; i++) {
-        dadosModel.pegarDados(i).then(resultado => {
-            res.json(resultado)
-        }).catch(
-            function (erro) {
+    dadosModel.pegarDados().then(resultado => {
+        console.log(resultado)
+    }).catch(
+        function (erro) {
 
-                res.status(500).json(erro);
+            res.status(500).json(erro);
 
-            }
-        );
-    }
+        }
+    );
 }
 
 module.exports = {
--- src/models/dadosModel.js
+++ src/models/dadosModel.js
@@ -1,7 +1,9 @@
 var database = require("../database/config");
 
-function pegarDados(fkPersonagem) {
-    var instrucaoSql = `SELECT COUNT(fkPersonagem) FROM usuario WHERE fkPersonagem = ${fkPersonagem};`
+function pegarDados() {
+    for (var i = 1; i <= 10; i++) {
+        var instrucaoSql = `SELECT COUNT(fkPersonagem) FROM usuario WHERE fkPersonagem = ${i};`
+    }
     return database.executar(instrucaoSql)
 }
 
