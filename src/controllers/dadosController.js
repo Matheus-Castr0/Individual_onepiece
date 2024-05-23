3fbbcdd54494712de782e4ae48fc356d7eb26f60
matheuscastro <matheus.csilva@sptech.school>
2024-05-23 13:38:01 -0300

    tratando erro de busca

--- src/controllers/dadosController.js
+++ src/controllers/dadosController.js
@@ -4,7 +4,13 @@ function qtdEscolhido(req, res) {
     for (var i = 1; i <= 10; i++) {
         dadosModel.pegarDados(i).then(resultado => {
             res.json(resultado)
-        })
+        }).catch(
+            function (erro) {
+
+                res.status(500).json(erro);
+
+            }
+        );
     }
 }
 
