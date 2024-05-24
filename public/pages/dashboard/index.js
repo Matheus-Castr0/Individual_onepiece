82a1dd2a0aed3d569c658fd857231acc0504486c
matheuscastro <matheus.csilva@sptech.school>
2024-05-23 21:16:04 -0300

    deixando grafico de barra dinamico

--- public/pages/dashboard/index.js
+++ public/pages/dashboard/index.js
@@ -4,7 +4,7 @@ window.onload = buscarQtdEscolhidos();
 function buscarQtdEscolhidos() {
     fetch("http://localhost:3333/banco/qtdEscolhidos").then(res => {
         res.json().then(response => {
-            console.log(response)
+            plotarGraficoBarra(response)
         })
     })
 }
@@ -24,41 +24,34 @@ function plotarKPIs() {
 
 }
 
-var graficoBarra = document.getElementById('graficoBar');
-var graficoDonut = document.getElementById('graficoDonut');
+function plotarGraficoBarra(dadosPersonagens) {
+    var personagens = []
+    var vezesEscolhidas = []
 
-new Chart(graficoBarra, {
-    type: 'bar',
-    data: {
-        labels: ['Luffy', 'Zoro', 'Nami', 'Usopp', 'Sanji', 'Chopper', 'Robin', 'Franky', 'Brook', 'Jinbe'],
-        datasets: [{
-            label: 'Personagem Escolhido como foto de Usuario',
-            data: [12, 19, 3, 5, 2, 3, 4, 1, 3, 9],
-            borderWidth: 1
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+    for (var i = 0; i <= 9 ; i++) {
+        personagens.push(dadosPersonagens[i].nome)
+        vezesEscolhidas.push(dadosPersonagens[i].escolhido)
+    }
+
+    var graficoBarra = document.getElementById('graficoBar');
+    
+    new Chart(graficoBarra, {
+        type: 'bar',
+        data: {
+            labels: personagens,
+            datasets: [{
+                label: 'Personagem Escolhido como foto de Usuario',
+                data: vezesEscolhidas,
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true
+                }
             }
         }
-    }
-});
+    });
 
-new Chart(graficoDonut, {
-    type: 'doughnut',
-    data: {
-        labels: ['Abaixo do Episodio 250', 'Entre o Episodio 250 e 500', 'Entre o Episodio 500 e 750', 'Acima do Episodio 1000'],
-        datasets: [{
-            data: [70, 50, 90, 30],
-            backgroundColor: [
-                'rgb(255, 99, 132)',
-                'rgb(54, 162, 235)',
-                'rgb(255, 205, 86)',
-                'rgb(15, 245, 36)'
-            ],
-            hoverOffset: 4
-        }]
-    }
-})
+}
